d74cecad4d42a047919f0e4745a6ddcd2c8939e4
steven5clu884 <steven5clu884@gmail.com>
2020-01-08 12:49:56 -0800

    First Commit
    A simple C# console .Net app that listens on localhost port 5000 that prints JSON, and save JSON to File

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace httpListener
+{
+    class Program
+    {
+		private static int messageCounter = 0;
+		static void Main(string[] args)
+		{
+			using (var listener = new HttpListener())
+			{
+				listener.Prefixes.Add("http://localhost:5000/");
+
+				listener.Start();
+
+				for (; ; )
+				{
+
+					Console.WriteLine("Listening...");
+
+					HttpListenerContext context = listener.GetContext();
+					HttpListenerRequest request = context.Request;
+					Console.WriteLine($"Total Length:{request.ContentLength64}  {request.ToString()}  {request.HttpMethod}");
+					string text;
+					using (var reader = new StreamReader(request.InputStream,
+														 request.ContentEncoding))
+					{
+						text = reader.ReadToEnd();
+					}
+					Console.WriteLine($"JSON:{text}");
+					using (System.IO.StreamWriter jsonFile = new System.IO.StreamWriter($"C:\\Extract\\listen{messageCounter}.json"))
+					{
+						jsonFile.WriteLine(text);
+					}
+					// TODO: read and parse the JSON data from 'request.InputStream'
+
+					using (HttpListenerResponse response = context.Response)
+					{
+						// returning some test results
+
+						// TODO: return the results in JSON format
+
+						string responseString = "<HTML><BODY>Hello, world!</BODY></HTML>";
+						byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+						response.ContentLength64 = buffer.Length;
+						using (var output = response.OutputStream)
+						{
+							output.Write(buffer, 0, buffer.Length);
+						}
+					}
+				}
+			}
+		}
+	}
+}
